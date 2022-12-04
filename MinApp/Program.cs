592a05acad1fe4cd01fb80b4bfd94e55beb4d0b6
andrey738 <76277376+andrey738@users.noreply.github.com>
2022-12-04 12:36:40 +0300

    Change2
    fix //

--- MinApp/Program.cs
+++ MinApp/Program.cs
@@ -10,9 +10,10 @@ namespace MinApp
     {
         static void Main(string[] args)
         {
-           // int a1 = int.Parse(Console.ReadLine());
-            //int a2 = int.Parse(Console.ReadLine());
-            //Console.WriteLine(a1 + a2);
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(a1 - a2);
+            Console.ReadLine();
 
 
         }
